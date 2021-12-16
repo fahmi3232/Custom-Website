affea43d500e9e7b91df1ebeced711392ac70e1f
fahmi3232 <fahimun32@gmail.com>
2021-12-16 07:38:26 +0700

    Update 15 Des

---
+++ src/component/class/Produk.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './CSS/Produk.css';
+
+
+class Produk extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            stock : this.props.stock,
+            sub : "Beli",
+            status : "Tersedia",
+            disabled : false
+
+        }
+    }
+    ButtonBeli = () => {
+        this.setState ({
+            stock: this.state.stock -1
+        })
+
+        if (this.state.stock ===1){
+            this.setState ({
+                status : "Habis",
+                disabled : true
+            })
+        }
+    }
+    render() {
+        return (
+            <div className='box-stock'>
+                <h2>{this.props.nama}</h2>
+                <img src="https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//101/MTA-7299263/apple_macbook_air_2020_512gb_full10_f3lxb660.jpg" alt="" />
+                <p><b>{this.props.harga}</b></p>
+                <p>{this.state.stock}</p>
+                <button className='btn-click' onClick={this.ButtonBeli} disabled={this.state.disabled}>Beli</button>
+                <p>{this.state.status}</p>
+                
+            </div>
+        )
+    }
+}
+
+export default Produk
---
+++ src/component/fungsi/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Produk from '../class/Produk';
+import Blog from './Blog';
+
+const Home = () => {
+    return <div>
+        { <Produk nama="Macbook Pro 2020" stock="10" harga="33.000.000"/>/* <Blog
+            tanggal="12 Desember 2021"
+            judul="Belajar HTML"
+            summary="Lorem ipsum, or lipsum as it is sometimes known, is dummy text used"
+        />
+        <Blog
+            tanggal="13 Desember 2021"
+            judul="Belajar CSS"
+            summary="Lorem ipsum, or lipsum as it is sometimes known, is dummy text used"
+        />
+        <Blog
+            tanggal="14 Desember 2021"
+            judul="Belajar Javascript"
+            summary="Lorem ipsum, or lipsum as it is sometimes known, is dummy text used"
+        />
+        <Blog
+            tanggal="15 Desember 2021"
+            judul="Belajar React JS Library"
+            summary="Lorem ipsum, or lipsum as it is sometimes known, is dummy text used"
+        /> */}
+    </div>
+}
+
+export default Home;
